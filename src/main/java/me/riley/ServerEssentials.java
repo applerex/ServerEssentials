a3c17fe3a2bf108bc57e8b668f0d04ac32f6f9c3
Riley <applerex2022@gmail.com>
2022-03-25 08:33:20 +0000

    Server Message

--- src/main/java/me/riley/ServerEssentials.java
+++ src/main/java/me/riley/ServerEssentials.java
@@ -1,5 +1,6 @@
 package me.riley;
 
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class ServerEssentials extends JavaPlugin {
@@ -7,11 +8,12 @@ public final class ServerEssentials extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "[ServerEssentials] Plugin has started");
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        getServer().getConsoleSender().sendMessage(ChatColor.RED + "[ServerEssentials] plugin has started");
     }
 }
